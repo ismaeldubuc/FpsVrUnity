628d71109c301e22e68b5829be2373162fb73d71
Ismaël Dubuc <125767744+ismaeldubuc@users.noreply.github.com>
2025-04-18 15:58:30 +0200

    spawn robot

---
+++ Assets/RobotSpawnController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class RobotSpawnController : MonoBehaviour
+{
+    public GameObject robotPrefab;
+    public float spawnInterval = 2f;
+    public float spawnDistance = 10f;
+    void Start()
+    {
+        InvokeRepeating("SpawnRobot", 2f, spawnInterval);
+    }
+    void SpawnRobot()
+    {
+        Vector3 spawnDirection = Random.onUnitSphere;
+        spawnDirection.y = Mathf.Clamp(spawnDirection.y, -0.5f, 0.5f);
+        Vector3 spawnPosition = transform.position + spawnDirection *
+            spawnDistance;
+        spawnPosition = new Vector3(spawnPosition.x, transform.position.y, spawnPosition.z);
+        GameObject robot = Instantiate(robotPrefab, spawnPosition,
+            Quaternion.identity);
+        
+    }
+}
--- Assets/collisionBall.cs
+++ Assets/collisionBall.cs
@@ -3,10 +3,22 @@ using UnityEngine;
 public class BulletCollision : MonoBehaviour
 {
     public Animator animator;
+    public RobotMover robotMover; 
     void OnTriggerEnter(Collider other)
     {
-        animator.Play("Hit");
-        Destroy(other.gameObject);
+        if (other.CompareTag("Bullet"))
+        {
+            animator.Play("Hit");
+            Destroy(other.gameObject);
+            robotMover.isDead = true;
+        }
+        else if(other.CompareTag("MainCamera"))
+        {
+            Hit();
+            //tu fait perdre les pv au joueur, parcequ'il a été touché par le robot
+        }
+        
+        
     }
 
     public void Hit()
